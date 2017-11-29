8dd23e65fbf41ef0df328a0a37d98fdd99a87134
Haegley <chaegley@cslab16.cs.skidmore.edu>
2017-11-29 15:31:00 -0500

    added a button for andrew ~<3

--- BirdBuddy/app/src/main/java/com/birdboys/skidmore/birdbuddy/MapActivity.java
+++ BirdBuddy/app/src/main/java/com/birdboys/skidmore/birdbuddy/MapActivity.java
@@ -1,8 +1,11 @@
 package com.birdboys.skidmore.birdbuddy;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,6 +30,16 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        Button localButton = (Button) findViewById(R.id.LocalButton);
+        localButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                
+            }
+        });
     }
 
 
